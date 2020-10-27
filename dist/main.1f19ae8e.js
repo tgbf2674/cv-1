e250ebc0b0423ed49faf3564b02165d7ad48ccdd
tgbf2674 <tgbf2020@163.com>
2020-10-27 19:13:53 +0800

    update bulid

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -176,7 +176,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55711" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64707" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -352,4 +352,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../../../AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js","main.js"], null)
-//# sourceMappingURL=/main.1f19ae8e.js.map
+//# sourceMappingURL=dist/main.1f19ae8e.js.map
